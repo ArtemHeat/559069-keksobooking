1bf0c5b8ba91b2d3e59c8407ba9751063401e587
Артем <art-dikov@yandex.ru>
2018-02-27 14:06:31 +0400

    24 Личный проект: перламутровые пуговицы

--- js/filtrate-similar-ads.js
+++ js/filtrate-similar-ads.js
@@ -1,160 +1,138 @@
 'use strict';
 window.filtrateSimilarAds = function () {
-  // var typeFiltrator;
-  // var priceFiltrator;
-  // var roomFiltrator;
-  // var guestFiltrator;
-  // var wifiFiltrator;
-  // var dishwasherFiltrator;
-  // var parkingFiltrator;
-  // var washerFiltrator;
-  // var elevatorFiltrator;
-  // var conditionerFiltrator;
   var form = document.querySelector('.map__filters');
   window.similarAds = [];
 
-  var typeSelectValueToTypeFilter = {
-    'any': function () {
-      return true;
-    },
-    'flat': function (item) {
-      return item.offer.type === 'flat';
-    },
-    'house': function (item) {
-      return item.offer.type === 'house';
-    },
-    'bungalo': function (item) {
-      return item.offer.type === 'bungalo';
-    }
+  var typeSelectValue = form.querySelector('#housing-type');
+  var priceSelectValue = form.querySelector('#housing-price');
+  var roomSelectValue = form.querySelector('#housing-rooms');
+  var guestSelectValue = form.querySelector('#housing-guests');
+
+  var wifiCheckboxValue = form.querySelector('#filter-wifi');
+  var dishCheckboxValue = form.querySelector('#filter-dishwasher');
+  var parkingCheckboxValue = form.querySelector('#filter-parking');
+  var washerCheckboxValue = form.querySelector('#filter-washer');
+  var elevatorCheckboxValue = form.querySelector('#filter-elevator');
+  var conditionerCheckboxValue = form.querySelector('#filter-conditioner');
+
+  var typeFilter = function (value) {
+    return function (item) {
+      if (value === 'any') {
+        return true;
+      } else {
+        return item.offer.type === value;
+      }
+    };
   };
 
-  var priceSelectValueToPriceFilter = {
-    'any': function () {
-      return true;
-    },
-    'middle': function (item) {
-      return item.offer.price > 10000 ||
-        item.offer.price < 50000;
-    },
-    'low': function (item) {
-      return item.offer.price < 10000;
-    },
-    'high': function (item) {
-      return item.offer.price >= 50000;
-    }
+  var priceFilter = function (value) {
+    return function (item) {
+      if (value === 'any') {
+        return true;
+      } else if (value === 'middle') {
+        return item.offer.price > 10000 && item.offer.price < 50000;
+      } else if (value === 'low') {
+        return item.offer.price < 10000;
+      } else {
+        return item.offer.price >= 50000;
+      }
+    };
   };
 
-  var roomSelectValueToRoomFilter = {
-    'any': function () {
-      return true;
-    },
-    '1': function (item) {
-      return item.offer.rooms === 1;
-    },
-    '2': function (item) {
-      return item.offer.rooms === 2;
-    },
-    '3': function (item) {
-      return item.offer.rooms === 3;
-    }
+  var roomFilter = function (value) {
+    return function (item) {
+      if (value === 'any') {
+        return true;
+      } else {
+        return item.offer.rooms === +value;
+      }
+    };
   };
 
-  var guestSelectValueToGuestFilter = {
-    'any': function () {
-      return true;
-    },
-    '1': function (item) {
-      return item.offer.guests === 1;
-    },
-    '2': function (item) {
-      return item.offer.guests === 2;
-    }
+  var guestFilter = function (value) {
+    return function (item) {
+      if (value === 'any') {
+        return true;
+      } else {
+        return item.offer.guests === +value;
+      }
+    };
   };
 
-  var wifiCheckboxValueToWifiFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('wifi') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var wifiFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('wifi') !== -1;
+      } else {
+        return true;
+      }
+    };
   };
 
-  var dishCheckboxValueToDishFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('dishwasher') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var dishwasherFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('dishwasher') !== -1;
+      } else {
+        return true;
+      }
+    };
   };
 
-  var parkingCheckboxValueToParkingFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('parking') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var parkingFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('parking') !== -1;
+      } else {
+        return true;
+      }
+    };
   };
 
-  var washerCheckboxValueToWasherFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('washer') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var washerFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('washer') !== -1;
+      } else {
+        return true;
+      }
+    };
   };
 
-  var elevatorCheckboxValueToElevatorFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('elevator') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var elevatorFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('elevator') !== -1;
+      } else {
+        return true;
+      }
+    };
   };
 
-  var conderCheckboxValueToConderFilter = {
-    'true': function (item) {
-      return item.offer.features.indexOf('conditioner') !== -1;
-    },
-    'false': function () {
-      return true;
-    }
+  var conditionerFilter = function (value) {
+    return function (item) {
+      if (value === 'true') {
+        return item.offer.features.indexOf('conditioner') !== -1;
+      } else {
+        return true;
+      }
+    };
+  };
+
+  var allFilters = function (value1, value2, value3, value4, value5,
+      value6, value7, value8, value9, value10) {
+    return typeFilter(value1) && priceFilter(value2) && roomFilter(value3) &&
+        guestFilter(value4) && wifiFilter(value5) && dishwasherFilter(value6) &&
+        parkingFilter(value7) && washerFilter(value8) && elevatorFilter(value9) &&
+        conditionerFilter(value10);
   };
 
   var updatePins = function () {
-    var typeFilter = typeSelectValueToTypeFilter[form.
-        querySelector('#housing-type').value];
-    var priceFilter = priceSelectValueToPriceFilter[form.
-        querySelector('#housing-price').value];
-    var roomFilter = roomSelectValueToRoomFilter[form.
-        querySelector('#housing-rooms').value];
-    var guestFilter = guestSelectValueToGuestFilter[form.
-        querySelector('#housing-guests').value];
 
-    var wifiFilter = wifiCheckboxValueToWifiFilter[form.
-        querySelector('#filter-wifi').checked];
-    var dishwasherFilter = dishCheckboxValueToDishFilter[form.
-        querySelector('#filter-dishwasher').checked];
-    var parkingFilter = parkingCheckboxValueToParkingFilter[form.
-        querySelector('#filter-parking').checked];
-    var washerFilter = washerCheckboxValueToWasherFilter[form.
-        querySelector('#filter-washer').checked];
-    var elevatorFilter = elevatorCheckboxValueToElevatorFilter[form.
-        querySelector('#filter-elevator').checked];
-    var conditionerFilter = conderCheckboxValueToConderFilter[form.
-        querySelector('#filter-conditioner').checked];
-
-    var allFilters = function (item) {
-      return typeFilter(item) && priceFilter(item) && roomFilter(item) &&
-        guestFilter(item) && wifiFilter(item) && dishwasherFilter(item) &&
-        parkingFilter(item) && washerFilter(item) && elevatorFilter(item) && conditionerFilter(item);
-    };
-
-    window.fillMapPins(window.similarAds.slice().filter(allFilters));
+    window.fillMapPins(window.similarAds.slice().filter(allFilters(typeSelectValue.value,
+        priceSelectValue.value, roomSelectValue.value, guestSelectValue.value, wifiCheckboxValue.checked,
+        dishCheckboxValue.checked, parkingCheckboxValue.checked, washerCheckboxValue.checked,
+        elevatorCheckboxValue.checked, conditionerCheckboxValue.checked)));
   };
 
   form.addEventListener('change', function (evt) {
